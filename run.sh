fa22578e16344ac8c6306fc752491d800c299431
Thomas Fricke <github@thomasfricke.de>
2015-10-23 17:34:45 +0200

    eye candy

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-docker run -t -i -h openvpn --name openvpn --dns=8.8.8.8 --cap-add=NET_ADMIN --device /dev/net/tun -v /home/thomas/import/openvpn-client/vpn:/vpn -v /media/space/incoming:/incoming -p 6886:6886 3dbe80a865d7 /bin/bash
+docker run -t -i -h openvpn \
+  --name openvpn \
+  --dns=8.8.8.8 \
+  --cap-add=NET_ADMIN \
+  --device /dev/net/tun \
+  -v $(pwd)/vpn:/vpn \
+  -v /media/space/incoming:/incoming \
+  -p 6886:6886 3dbe80a865d7 \
+  /bin/bash
